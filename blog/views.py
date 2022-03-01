ce1b6897fdd5521deb257a074314fa5d04892699
Damian Jacob <damianm.jacob@yahoo.com>
2022-03-01 10:31:53 +0000

    deployment commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -46,8 +48,9 @@ class PostDetail(View):
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
-            comment_form.post = post
+            comment.post = post
             comment.save()
+            messages.success(request, 'Your comment has been submitted succesfully')
         comment_form = CommentForm()
 
         return render(
@@ -61,3 +64,15 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+
+class PostLike(View):
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+            
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
